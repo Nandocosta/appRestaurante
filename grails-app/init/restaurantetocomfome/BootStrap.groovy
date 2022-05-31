1f9e6fa9ee137768fe2c5b46cc75ffff5b8e579f
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-05-31 13:06:54 -0300

    Adicionando spring security core, e página de logar

--- grails-app/init/restaurantetocomfome/BootStrap.groovy
+++ grails-app/init/restaurantetocomfome/BootStrap.groovy
@@ -1,8 +1,14 @@
 package restaurantetocomfome
 
+import comum.Permissao
+
 class BootStrap {
 
     def init = { servletContext ->
+        Permissao admin = Permissao.findByAuthority("ADMIN")
+        if(admin == null){
+            admin = new Permissao(authority: "ADMIN").save(flush: true)
+        }
     }
     def destroy = {
     }
